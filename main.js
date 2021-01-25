7d2bfa4ca7205c665dff05912faca5773956d411
Chhandanghshi-Ghosh <73993378+Chhandanghshi-Ghosh@users.noreply.github.com>
2021-01-25 17:35:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300,300)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+}
+
+function takeSnapshot(){
+    save("mybirthdaypicture.jpg")
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    fill("red");
+    stroke("black")
+    circle(20);
+    }
